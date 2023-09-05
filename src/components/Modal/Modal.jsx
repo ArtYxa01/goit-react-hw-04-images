bdbc21c0d693e5aef39c169372b72ab75d0e6488
ArtYxa01 <115730030+ArtYxa01@users.noreply.github.com>
2023-09-05 22:45:18 +0300

    fghjkl

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import { Overlay, ModalViewer, ModalImg } from './Modal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export const Modal = ({ closeModal, tags, modalImg }) => {
+
+  
+
+  useEffect(() => {
+    const closeByEsc = e => {
+      if (e.code !== 'Escape') {
+        return;
+      }
+      closeModal();
+    };
+
+    window.addEventListener('keydown', closeByEsc);
+
+    return () => {
+      window.removeEventListener('keydown', closeByEsc);
+    };
+  }, [closeModal]);
+
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      closeModal();
+    }
+   
+  };
+        
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      <ModalViewer>
+        <ModalImg src={modalImg} alt={tags} />
+      </ModalViewer>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+
+Modal.propTypes = {
+  modalImg: PropTypes.string.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  tags: PropTypes.string.isRequired,
+  // handleBackdropClick: PropTypes.func.isRequired,
+}
